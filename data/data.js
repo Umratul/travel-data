5d3f75baf44275fc7826e8fd03301a7c46e2cc54
Umratul <umratul0904@gmail.com>
2021-11-05 12:39:39 +0600

    first commit

---
+++ data/data.js
@@ -0,0 +1,59 @@
+const details = [
+    {
+        id: 01,
+        name: 'Italy',
+        img: '',
+        duration: '3 Days 2 Nights',
+        price: 78.00,
+        body: ''  
+    },
+    {
+        id: 02,
+        name: 'Paris',
+        img: '',
+        duration: '5 Days 4 Nights',
+        price: 95.00,
+        body: ''  
+    },
+    {
+        id: 02,
+        name: 'Paris',
+        img: '',
+        duration: '5 Days 4 Nights',
+        price: 95.00,
+        body: ''  
+    },
+    {
+        id: 03,
+        name: 'Cuba',
+        img: '',
+        duration: '5 Days 4 Nights',
+        price: 87.00,
+        body: ''  
+    },
+    {
+        id: 04,
+        name: 'Canada',
+        img: '',
+        duration: '7 Days 6 Nights',
+        price: 104.00,
+        body: ''  
+    },
+    {
+        id: 05,
+        name: 'Brazil',
+        img: '',
+        duration: '10 Days',
+        price: 170.00,
+        body: ''  
+    },
+    {
+        id: 06,
+        name: 'Turkey',
+        img: '',
+        duration: '3 Days 2 Nights',
+        price: 65.00,
+        body: ''  
+    }
+    
+]
