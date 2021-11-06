d9ba69540b59059dd79be05d866c4ae34bae6d21
Umratul <umratul0904@gmail.com>
2021-11-06 21:30:38 +0600

    1

--- data/data.js
+++ data/data.js
@@ -2,23 +2,16 @@ const details = [
     {
         id: 01,
         name: 'Italy',
-        img: '',
+        img: 'https://image.freepik.com/free-photo/beautiful-aerial-shot-florence-italy-architecture-evening_181624-2203.jpg',
         duration: '3 Days 2 Nights',
         price: 78.00,
-        body: ''  
+        body: "lorem10"
     },
+
     {
         id: 02,
         name: 'Paris',
-        img: '',
-        duration: '5 Days 4 Nights',
-        price: 95.00,
-        body: ''  
-    },
-    {
-        id: 02,
-        name: 'Paris',
-        img: '',
+        img: 'https://image.freepik.com/free-photo/famous-eiffel-tower-paris-with-gorgeous-colors_268835-830.jpg',
         duration: '5 Days 4 Nights',
         price: 95.00,
         body: ''  
@@ -26,7 +19,7 @@ const details = [
     {
         id: 03,
         name: 'Cuba',
-        img: '',
+        img: 'https://image.freepik.com/free-photo/authentic-view-street-old-havana-with-old-buildings-cars_109800-8727.jpg',
         duration: '5 Days 4 Nights',
         price: 87.00,
         body: ''  
@@ -34,7 +27,7 @@ const details = [
     {
         id: 04,
         name: 'Canada',
-        img: '',
+        img: 'https://image.freepik.com/free-photo/vancouver-science-world_198523-43.jpg',
         duration: '7 Days 6 Nights',
         price: 104.00,
         body: ''  
@@ -42,7 +35,7 @@ const details = [
     {
         id: 05,
         name: 'Brazil',
-        img: '',
+        img: 'https://image.freepik.com/free-photo/vertical-shot-building-with-colonial-architecture-sao-luis-brazil_181624-44427.jpg',
         duration: '10 Days',
         price: 170.00,
         body: ''  
@@ -50,7 +43,7 @@ const details = [
     {
         id: 06,
         name: 'Turkey',
-        img: '',
+        img: 'https://image.freepik.com/free-photo/istanbul-s-ocean-with-cruise-ship_53876-42289.jpg',
         duration: '3 Days 2 Nights',
         price: 65.00,
         body: ''  
